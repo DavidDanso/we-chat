e48e436d7d10ddc0585f9498dc56035cbe50b08a
DavidDanso <desiingerboy@icloud.com>
2024-05-04 01:13:12 +0000

    create models for GroupMessage & GroupName

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from a_user.models import Profile
+# import shortuuid
 
-# Create your models here.
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=128, unique=True)
+    
+    def __str__(self):
+        return self.group_name
+
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.author.username} : {self.body}'
+    
+    class Meta:
+        ordering = ['-created']
